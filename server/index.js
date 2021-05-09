6685ff3ca51df948aab8fede6d517fdb48f413a6
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-05-09 12:53:46 +0200

    pull from github

--- server/index.js
+++ server/index.js
@@ -67,7 +67,7 @@ app.post("/refresh", (req, res) => {
 //#endregion
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
 app.listen(PORT, () => {
