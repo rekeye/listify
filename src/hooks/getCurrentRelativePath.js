a22046806fb102bebb25813ebcc50879987e7fa3
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-05-11 10:58:35 +0200

    resolve #7: added a filter for search variables in getCurrentRelativePath hook

--- src/hooks/getCurrentRelativePath.js
+++ src/hooks/getCurrentRelativePath.js
@@ -1,6 +1,7 @@
 export default function getCurrentRelativeURL() {
   const currentURL = window.location.href;
   const hostURL = `${window.location.protocol}//${window.location.host}`;
+  const searchVars = window.location.search;
 
-  return currentURL.replace(hostURL, "");
+  return currentURL.replace(hostURL, "").replace(searchVars, "");
 }
