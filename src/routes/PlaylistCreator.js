d75df4442da53d16ba9b53ec3b46615860f6b0e6
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-05-16 15:23:18 +0200

    feat #9 - creating playlist creator route

---
+++ src/routes/PlaylistCreator.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PlaylistCreator = () => {
+  return (
+    <>
+      <form></form>
+      <section></section>
+    </>
+  );
+};
+
+export default PlaylistCreator;
