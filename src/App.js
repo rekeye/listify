20f33cb5b63712efca70a695a110b51cf0fa1347
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-05-16 15:23:42 +0200

    feat #9 - installing Formik

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
